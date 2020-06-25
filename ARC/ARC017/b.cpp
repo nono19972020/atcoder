05b659f530e32c73e9d65c85a008d36fc9dc4f98
naoaki <naoaki.galaxy@gmail.com>
2020-06-25 16:56:43 +0900

    20200625

--- ARC/ARC017/b.cpp
+++ ARC/ARC017/b.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 #include <cstdio>
+#include <vector>
 using namespace std;
 
 int main(){
+    long long n ,k;
+    cin >> n >> k;
+
+    vector<long long> a(n);
+
+    for(long long i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    long long late_low_index = -1;
+    for(long long i = 1; i < k; i++){
+        if(a[i] - a[i-1] <= 0){
+            late_low_index = i;
+        }
+    }
     
+    long long count = 0;
+    for(long long i = 0; i + k - 1 < n; i++){
+        if(a[i+k-1] - a[i+k-2] <= 0){
+            late_low_index = i+k-1;
+        }
+
+        if(late_low_index <= i){
+            count++;
+        }
+    }
+
+    cout << count << endl;
+    return 0;
 }
