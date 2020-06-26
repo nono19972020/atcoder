a0096a902cc868fdc6e25a9ba1a3b77ed528f263
naoaki <naoaki.galaxy@gmail.com>
2020-06-26 13:03:46 +0900

    20200626

--- code_fes/code_fes_2015_middle/a.cpp
+++ code_fes/code_fes_2015_middle/a.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 #include <cstdio>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main(){
+    long long n, k, m, r;
+    cin >> n >> k >> m >> r;
+
+    vector<long long> s(n);
+    for(long long i = 0; i < n-1; i++){
+        cin >> s[i];
+    }
+
+    sort(s.begin(), s.end(), greater<long long>());
     
+    long long sum = 0;
+
+    for(long long i = 0; i < k; i++){
+        sum += s[i];
+    }
+
+    if(sum >= r * k){
+        printf("0\n");
+        return 0;
+    }
+
+    sum -= s[k-1];
+    long long p = r*k - sum;
+    if(0 <= p and p <= m){
+        printf("%lld\n", p);
+    }
+    else{
+        printf("-1\n");
+    }
+   
+    return 0;
 }
