31610955dbbb12adf530ada251c66cfbc72c89b4
naoaki <naoaki.galaxy@gmail.com>
2020-06-29 11:25:45 +0900

    20200629

--- tenka1/2018/c.cpp
+++ tenka1/2018/c.cpp
@@ -14,5 +14,52 @@ int main(){
         cin >> a[i];
     }
 
-    
+    sort(a.begin(), a.end(), greater<long long>());
+    long long sum = 0;
+    if(n % 2 != 0){
+        long long sum1 = 0;
+
+        for(long long i = 0; i < n; i++){
+            if(i < (n-1)/2){
+                sum1 += 2*a[i];
+            }
+            else if((n-1)/2 <= i and i <= (n+1)/2){
+                sum1 -= a[i];
+            }
+            else{
+                sum1 -= 2*a[i];
+            }
+        }
+        long long sum2 = 0;
+        for(long long i = 0; i < n; i++){
+            if(i < (n-3)/2){
+                sum2 += 2*a[i];
+            }
+            else if((n-3)/2 <= i and i <= (n-1)/2){
+                sum2 += a[i];
+            }
+            else{
+                sum2 -= 2*a[i];
+            }
+        }
+        sum = max(sum1, sum2);
+    }
+    else{
+        for(long long i = 0; i < n; i++){
+            if(i < (n-2)/2){
+                sum += 2*a[i];
+            }
+            else if((n-2)/2 == i ){
+                sum += a[i];
+            }
+            else if(n/2 == i){
+                sum -= a[i];
+            }
+            else{
+                sum -= 2*a[i];
+            }
+        }
+    }
+    cout << sum << endl;
+    return 0;
 }
