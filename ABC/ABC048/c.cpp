4f7766639d167e189365e0f1c7f7c7b2dc297f2a
naoaki <naoaki.galaxy@gmail.com>
2020-07-01 11:16:24 +0900

    20200701

--- ABC/ABC048/c.cpp
+++ ABC/ABC048/c.cpp
@@ -3,5 +3,29 @@
 using namespace std;
 
 int main(){
-    
+    long long n, x;
+    cin >> n >> x;
+    long long a[n];
+    for(long long i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    long long count = 0;
+    for(long long i = 0; i < n-1; i++){
+        if(a[i] + a[i+1] > x){
+            long long delta = a[i] + a[i+1] - x;
+            count += delta;
+            if(a[i+1] - delta >= 0){
+                a[i+1] -= delta;
+            }
+            else{
+                delta -= a[i+1];
+                a[i+1] = 0;
+                a[i] -= delta;
+            }
+        }
+    }
+
+    cout << count << endl;
+    return 0;
 }
