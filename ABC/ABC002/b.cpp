bc5557ecd1a8365d40baeb9aafe3b37f750ebb50
naoaki114514 <naoaki.galaxy@gmail.com>
2020-07-25 13:51:20 +0900

    20200725

---
+++ ABC/ABC002/b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+    string w;
+    cin >> w;
+
+    string ans;
+
+    for(long long i = 0; i < w.size(); i++){
+        if(w[i] == 'a' || w[i] == 'i' || w[i] == 'u' || w[i] == 'e' || w[i] == 'o') continue;
+        ans.push_back(w[i]);
+    }
+
+    cout << ans << endl;
+    return 0;
+}
