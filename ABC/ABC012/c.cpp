b19f9a7f47676c08b0acb7920415378c581722b5
naoaki <naoaki.galaxy@gmail.com>
2020-07-25 11:22:33 +0900

    20200725

--- ABC/ABC012/c.cpp
+++ ABC/ABC012/c.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <cstdio>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
+vector<long long> divisor(long long n) {
+    vector<long long> ret;
+    for (long long i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            ret.push_back(i);
+            if (i * i != n) ret.push_back(n / i);
+        }
+    }
+    sort(ret.begin(), ret.end());
+    return ret;
+}
 
 int main(){
     long long n;
@@ -15,4 +29,13 @@ int main(){
     }
 
     long long m = sum - n;
+
+    vector<long long> ret;
+
+    ret = divisor(m);
+
+    for(long long i = 0; i < ret.size(); i++){
+        if(ret[i] < 10 and m/ret[i] < 10) printf("%lld x %lld\n", ret[i], m/ret[i]);
+    }
+    return 0;
 }
