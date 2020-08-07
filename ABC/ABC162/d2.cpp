5ace20d04e35e959a3c652cf51adafaa563d6a37
naoaki <naoaki.galaxy@gmail.com>
2020-08-07 15:38:42 +0900

    20200807 second cant

---
+++ ABC/ABC162/d2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+    long long n;
+    string s;
+    cin >> n;
+    cin >> s;
+
+    long long cnt_r = 0, cnt_g = 0, cnt_b = 0;
+
+    for(long long i = 0; i < n; i++){
+        if(s[i] == 'R') cnt_r++;
+        if(s[i] == 'G') cnt_g++;
+        if(s[i] == 'B') cnt_b++;
+    }
+
+    long long cnt = 0;
+    for(long long i = 0; i < n; i++){
+        for(long long j = 1; i+2*j < n ;j++){
+            if(s[i] != s[i+j] and s[i+j] != s[i+2*j] and s[i+2*j] != s[i]) cnt++;
+        }
+    }
+
+    cout << cnt_r * cnt_g * cnt_b  - cnt << endl;
+    return 0;
+}
