040bf9ddfa783cc9a610958e0b050c33b49d0698
naoaki <naoaki.galaxy@gmail.com>
2020-08-22 17:01:38 +0900

    conbnation

--- library/1.cpp
+++ library/1.cpp
@@ -228,6 +228,21 @@ long long comb(long long n, long long k){
     return x*y%mod;//計算可能
 }
 
+/*modを使わないコンビネーション　遅いかも*/
+long long comb(int n, int r) {
+  vector<vector<long long> > v(n + 1,vector<long long>(n + 1, 0));
+  for (int i = 0; i < v.size(); i++) {
+    v[i][0] = 1;
+    v[i][i] = 1;
+  }
+  for (int j = 1; j < v.size(); j++) {
+    for (int k = 1; k < j; k++) {
+      v[j][k] = (v[j - 1][k - 1] + v[j - 1][k]);
+    }
+  }
+  return v[n][r];
+}
+
 /*約数を求める*/
 vector<long long> divisor(long long n) {
     vector<long long> ret;
