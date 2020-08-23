652c1f53fec7a594441aa0d8b241bf1ee077d07c
naoaki <naoaki.galaxy@gmail.com>
2020-08-23 23:23:34 +0900

    2020-08-23#日付けでcommit

--- git_push.sh
+++ git_push.sh
@@ -2,5 +2,6 @@
 stdout=`date +%F` #get date as yyyy-mm-dd 標準出力を受け取る
 echo $stdout
 
-git add .
-git commit -m "$stdout"
+git add . #差分をadd
+git commit -m "$stdout"#日付けでcommit
+git push -u origin master #git push
