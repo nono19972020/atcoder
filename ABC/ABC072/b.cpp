e747900b735cc03d280943785abdc78ed7e1e24f
nono19972020 <naoaki.galaxy@gmail.com>
2020-09-25 21:42:28 +0900

    2020-09-25

--- ABC/ABC072/b.cpp
+++ ABC/ABC072/b.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 int main(){
-    
+    string s;
+    cin >> s;
+
+    string ans;
+
+    for(long long i = 0; i < s.size(); i++){
+        if(i % 2 == 0) ans.push_back(s[i]);
+    }
+
+    cout << ans << endl;
+    return 0;
 }
