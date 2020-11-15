658576ad81abdf17992bcab75be9d22dc1692f92
nono19972020 <naoaki.galaxy@gmail.com>
2020-11-15 12:13:47 +0900

    2020-11-15

---
+++ ABC/ABC113/a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+    long long x, y;
+    cin >> x >> y;
+
+    long long ans = x + y/2;
+
+    cout << ans << endl;
+    return 0;
+}
